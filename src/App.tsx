f4c7d3c72c172ef24e60916a787b92bcb72747bb
bobybox <vesa.basha1@gmail.com>
2026-06-01 00:43:49 +0200

    scroll to top fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/index'
 import Exploring from './pages/exploring'
 import Hobbies from './pages/hobbies'
 import AppBackground from './components/AppBackground'
 import './App.css'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="relative min-h-screen bg-black text-white">
         <div className="pointer-events-none absolute inset-0 z-0 overflow-hidden">
           <AppBackground />
